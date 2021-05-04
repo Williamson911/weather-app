4fa7a5773bb7eae5a549cda497167f83d7af237e
Williamson911 <william.williamson.91@gmail.com>
2021-05-04 16:25:43 +0200

    app and style

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const api = {
+    key: "428dd08071d291463fc744ae1b9d59e7",
+    base: "https://home.openweathermap.org/data/2.5/"
+}
+
+function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+    return (
+        <div className="app">
+            <main>
+                <div className="search-box">
+                    <input
+                    type="text"
+                    className="search-bar"
+                    placeholder="Search..."
+                    />
+                </div>
+                <div className="location-box">
+                  <div className="location">New York City, US</div>
+                  <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+                <div className="waether-box">
+                  <div className="temp">
+                    15°c
+                  </div>
+                  <div className="weather">
+                    Sunny
+                  </div>
+                </div>
+            </main>
+
+        </div>
+    );
+}
+
+export default App;
